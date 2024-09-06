ed2b5c25a298f6f743825dd950c8edcb77f6be97
Maider <maiderbarrutia@hotmail.com>
2024-09-06 18:38:46 +0200

    add footer

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,12 +1,11 @@
 import styles from './Footer.module.scss'; 
 function Footer() {
+  const currentYear = new Date().getFullYear();
+  
     return (
-        <>
-          <footer className={styles.footer}>
-            Footer
-          </footer>
-          
-        </>
-      )
+      <footer className={styles.footer}>
+        <p>© {currentYear} | Diseñado y codificado por Maider Barrutia | <a href="/politica-de-privacidad">POLÍTICA DE PRIVACIDAD</a></p>
+      </footer>
+    )
 }
 export default Footer
